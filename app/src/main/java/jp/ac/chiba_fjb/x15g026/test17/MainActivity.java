08236847eb2292d0a31a9fc16849c10b19a1879c
WIN_NT\x15g026 <x15g026@chiba-fjb.ac.jp>
2017-05-23 10:58:02 +0900

    レイアウトの多重化

---
+++ app/src/main/java/jp/ac/chiba_fjb/x15g026/test17/MainActivity.java
@@ -0,0 +1,31 @@
+package jp.ac.chiba_fjb.x15g026.test17;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        View view  = findViewById(R.id.include2);
+//        TextView textView = (TextView) view.findViewById(R.id.textView);
+//        textView.setText("テキスト設定");
+
+        LinearLayout output = (LinearLayout) findViewById(R.id.output);
+        LinearLayout layout;
+        layout = (LinearLayout) getLayoutInflater().inflate(R.layout.layout1,null);
+        output.addView(layout);
+        layout = (LinearLayout) getLayoutInflater().inflate(R.layout.layout1,null);
+        output.addView(layout);
+        layout = (LinearLayout) getLayoutInflater().inflate(R.layout.layout1,null);
+        output.addView(layout);
+        layout = (LinearLayout) getLayoutInflater().inflate(R.layout.layout1,null);
+        output.addView(layout);
+    }
+}
